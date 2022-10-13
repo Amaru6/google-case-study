f7a10e7cd61419887bc1f2910537a57020f167f0
Amarullazo <julio.collazos@unmsm.edu.pe>
2022-10-13 16:17:45 -0500

    added modelling

--- analysis/analysis_2_13-10-2022.R
+++ analysis/analysis_2_13-10-2022.R
@@ -4,3 +4,68 @@ library(tidymodels)
 source(here("functions", "functions_1.R"))
 theme_set(theme_julio())
 
+# data ------#
+read_rds(here("data", "workfile", "workfile.rds")) -> workfile_data
+#----------#
+decision_tree(tree_depth = 3) |> 
+  set_engine("rpart") |> 
+  set_mode("regression") -> tree_spec
+
+workfile_data |>
+  separate(key, into = c("departamento", "provincia", "distrito"),
+           sep = "_") |> 
+  select(area, gestion_2, ise_mean, matematica, lectura,
+         departamento, provincia, distrito) |> 
+  mutate(across(.cols = c(matematica, lectura),
+                .fns = scale)) |> 
+  mutate(test = (matematica + lectura)/2) |> 
+  select(departamento, provincia, distrito, 
+         area, gestion_2, ise_mean, test) -> workfile_to_model
+workfile_to_model |> 
+  mutate(across(.cols = where(is.character),
+                .fns = as.factor)) -> workfile_to_model
+
+workfile_to_model |> 
+  recipe(test ~ ., data = _) |> 
+  step_dummy(all_nominal_predictors()) -> recipe_1
+
+workflow() |> 
+  add_recipe(recipe_1) |> 
+  add_model(tree_spec) -> wf_1
+
+### fit_model ------#
+wf_1 |> 
+  fit(workfile_to_model) -> tree_model_fitted
+
+tree_model_fitted |> 
+  extract_fit_engine() -> tree_model_rpart
+tree_model_rpart |> treemisc::tree_diagram()
+
+### From this simple model and the diagram we can clearly see that
+### one of the most important variables to consider is the economic
+### situation of the family. As mentioned above, all those confounders
+### seem to be important.
+
+
+
+
+### I can guess the most important factor is the economic condition
+### but we need to confirm that with data and come up with evidence.
+
+# GET THE MOST IMPORTANT PREDICTORS
+
+pacman::p_load(DALEXtra, DALEX)
+explain_tidymodels(
+  model = tree_model_fitted,
+  data = workfile_to_model |> select(-test),
+  y = workfile_to_model |> pull(test),
+  label = "rpart_model"
+) -> model_explained
+set.seed(6)
+model_parts(model_explained) -> mod_var_imp
+plot(mod_var_imp)  # Confirmed.
+
+
+
+
+
